d427b7b794a8908fa52c49c4775aad8be782cbee
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-07-05 09:13:49 +0200

    And example of the script has been added

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
 require_relative 'lib/office_of_energy_scraper'
+
+office_of_energy_scraper = OfficeOfEnergyScraper.new(
+  db_username: 'USERNAME',
+  db_password: 'PASSWORD',
+  db_name: 'DB_NAME',
+  host: 'localhost',
+  scrape_dev: 'SCRAPE_DEV'
+)
+
+office_of_energy_scraper.start
